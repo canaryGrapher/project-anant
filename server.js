3dd44b60ccffb0c7f752149f6b0f4fa253ee2057
Yash Aryan <yasharyan307@outlook.com>
2022-04-10 23:18:57 +0530

    Feature: Added Helmet for securing backend

--- server.js
+++ server.js
@@ -3,11 +3,19 @@ const next = require('next')
 const bodyParser = require('body-parser')
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
+const helmet = require("helmet");
 const handle = app.getRequestHandler()
 
+app.use(express.json())
+
+app.use(
+    helmet({
+        contentSecurityPolicy: false,
+    })
+);
+
+
 const main = async () => {
-
-
     app.prepare().then(() => {
         const server = express()
         server.use(bodyParser.json())
