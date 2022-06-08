6c40255d50f1ce6807663844a875032334661144
Yash Aryan <yasharyan307@outlook.com>
2022-06-09 04:56:01 +0530

    Minor fix

--- components/home/Apps/Apps.js
+++ components/home/Apps/Apps.js
@@ -37,20 +37,25 @@ export default function Apps() {
           <div className="w-56 mx-auto my-2 h-1 bg-gray-100"></div>
         </div>
         <div className="grid lg:grid-cols-3 grid-cols-1 w-5/6 mx-auto gap-x-5 gap-y-3">
-          {AppData.map(app => {
-            const container = app.link ? (<Link href={app.link}>
-              <div className="block bg-gray-100 p-3 lg:p-5 rounded-lg cursor-pointer">
+          {AppData.map((app, index) => {
+            const container = app.link ?
+              (
+                <Link href={app.link} key={index}>
+                  <div className="block bg-gray-100 p-3 lg:p-5 rounded-lg cursor-pointer">
+                    <Image src={app.image} height="100" width="200" layout="responsive" />
+                    <h5 className="theme-text text-center md:text-2xl text-xl pt-2">{app.name}</h5>
+                    <div className="w-3/4 mx-auto theme" style={{ height: 2 }}></div>
+                    <p className="text-gray-900 md:text-lg text-md text-center pt-2">{app.description}</p>
+                  </div>
+                </Link>
+              )
+              :
+              <div className="opacity-50 block bg-gray-100 md:p-5 p-3 rounded-lg cursor-not-allowed" key={index}>
                 <Image src={app.image} height="100" width="200" layout="responsive" />
                 <h5 className="theme-text text-center md:text-2xl text-xl pt-2">{app.name}</h5>
                 <div className="w-3/4 mx-auto theme" style={{ height: 2 }}></div>
                 <p className="text-gray-900 md:text-lg text-md text-center pt-2">{app.description}</p>
               </div>
-            </Link>) : <div className="opacity-50 block bg-gray-100 md:p-5 p-3 rounded-lg cursor-not-allowed">
-              <Image src={app.image} height="100" width="200" layout="responsive" />
-              <h5 className="theme-text text-center md:text-2xl text-xl pt-2">{app.name}</h5>
-              <div className="w-3/4 mx-auto theme" style={{ height: 2 }}></div>
-              <p className="text-gray-900 md:text-lg text-md text-center pt-2">{app.description}</p>
-            </div>
             return container
           })
           }
