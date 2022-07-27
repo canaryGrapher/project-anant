0061362b463dc7cf23363a63fd49bfcbf04b9300
Parthiv-M <parthiv.menon@learner.manipal.edu>
2022-07-27 05:47:42 -0400

    added loading and error states for password reveal

--- components/common/Modal/index.js
+++ components/common/Modal/index.js
@@ -2,6 +2,8 @@ import { useState } from "react";
 
 export default function Modal({ isOpen, setIsOpen }) {
   const [email, setEmail] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
   const [isValidEmail, setIsValidEmail] = useState(undefined);
   const [showPassword, setShowPassword] = useState(false);
   const [isCopied, setIsCopied] = useState(false);
@@ -18,6 +20,7 @@ export default function Modal({ isOpen, setIsOpen }) {
 
   const submitEmail = () => {
     if (validateEmail(email)) {
+      setLoading(true);
       // send email to server and receive zip file password
       fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/email`, {
         method: "POST",
@@ -28,8 +31,12 @@ export default function Modal({ isOpen, setIsOpen }) {
           email: email,
         }),
       }).then(async (res) => {
-        const response = await res.json()
+        const response = await res.json();
+        if(!response) {
+          setIsError(true);
+        }
         if (res.status === 200) {
+          setIsError(false);
           setIsValidEmail(true);
           setShowPassword(true);
           setPassword(response.password);
@@ -37,12 +44,13 @@ export default function Modal({ isOpen, setIsOpen }) {
         }
       }).catch((err) => {
         console.log(err);
-        setIsValidEmail(false);
+        setIsError(true);
       }).finally(() => {
-        setIsValidEmail(true);
+        setLoading(false);
       })
     } else {
       setIsValidEmail(false);
+      setLoading(false);
     }
   }
 
@@ -66,20 +74,34 @@ export default function Modal({ isOpen, setIsOpen }) {
         />
         {isValidEmail && isValidEmail != undefined && <p className="text-sm text-green-600"><span><i className="fa fa-check mr-1"></i>Valid Email</span></p>}
         {!isValidEmail && isValidEmail != undefined && <p className="text-sm text-red-700"><span><i className="fa fa-exclamation-circle mr-1"></i></span>Invalid Email</p>}
-        {!isValidEmail && <button
+        {!isValidEmail && !isError && <button
           className="w-full theme mt-4 py-3 rounded-lg text-gray-100 outline-none hover:font-bold"
           onClick={submitEmail}
-        >Submit</button>}
+        >
+          { !loading ? "Submit" : <><span><i className="fa fa-circle-o-notch mr-2 animate-spin"></i></span><span>Verifying</span></>}
+        </button>}
+        {
+          isError && <div
+          className="w-full text-center bg-red-700 mt-4 py-3 rounded-sm text-gray-100 outline-none font-bold"
+          onClick={submitEmail}
+        >
+          <span><i className="fa fa-exclamation-triangle mr-2"></i></span>There seems to be an error, try later!
+        </div>
+        }
         <button
           className="w-full border border-gray-300 mt-2 py-3 rounded-lg text-gray-900 outline-none hover:font-bold"
-          onClick={() => { setIsOpen(false); setIsValidEmail(undefined); setShowPassword(false); }}
+          onClick={() => { setIsOpen(false); setIsValidEmail(undefined); setShowPassword(false); setIsError(false); setLoading(false) }}
         >Close</button>
       </div>
       {
         showPassword && isValidEmail && <div className="py-4 px-14 bg-gray-100 rounded-xl flex flex-col justify-center items-start">
           <p className="theme-text text-md">The password to open the <monospace>.zip</monospace> file is given below</p>
           <div className="w-full mt-1 px-3 py-2 bg-gray-700 text-gray-200 rounded-md flex justify-between items-center">
-            <pre>{password}</pre>
+            {
+              loading 
+              ? <><span><i className="fa fa-circle-o-notch mr-2 animate-spin"></i></span><span>Loading</span></> 
+              : <pre>{password}</pre>
+            }
             <div className="flex items-center">
               {isCopied && <p className="text-green-400 mr-1 text-sm">Copied</p>}
               {!isCopied && <i className="fa fa-copy hover:bg-gray-800 cursor-pointer p-2 rounded-full" onClick={copyToClipboard}></i>}
