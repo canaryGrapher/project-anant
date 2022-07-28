2c112ea53e2c5564064a6fb9b38e5935459452b8
Yash Aryan <yasharyan307@outlook.com>
2022-07-29 00:02:50 +0530

    Added new URL to next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 // next.config.js
 module.exports = {
     images: {
-        domains: ['ik.imagekit.io', 'localhost'],
+        domains: ['ik.imagekit.io', 'localhost', "api.mrc.iisc.ac.in"],
     },
 }
