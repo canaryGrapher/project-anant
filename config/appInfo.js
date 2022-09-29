9d7568e04a479140c51a2841f8275e9ef141eee1
Parthiv-M <parthiv.menon@learner.manipal.edu>
2022-09-29 16:42:34 +0530

    updated supertokens variables for prod

--- config/appInfo.js
+++ config/appInfo.js
@@ -1,8 +1,8 @@
 export const appInfo = {
     // learn more about this on https://supertokens.com/docs/thirdpartyemailpassword/appinfo
     appName: "Project Anant",
-    apiDomain: "http://localhost:3002",
-    websiteDomain: "http://localhost:3000",
+    apiDomain: "https://anantapi.mrc.iisc.ac.in",
+    websiteDomain: "https://project-anant.vercel.app",
     apiBasePath: "/",
     websiteBasePath: "/auth"
 }
