93efd9f0be776bb707659e94c8d1fc72b3714dd9
Parthiv-M <parthiv.menon@learner.manipal.edu>
2022-10-08 01:22:15 +0530

    modified full database download functionality

--- pages/apps/mxene/index.js
+++ pages/apps/mxene/index.js
@@ -45,13 +45,20 @@ export default function Mxene() {
                     <Fragment>
                         {
                             loggedIn ?
-                                <button
-                                    onClick={() => setIsOpen(true)}
-                                    className="w-full lg:w-auto theme bg-gray-300 theme-text rounded-md text-lg px-4 py-3 hover:translate-y-1 outline-none"
+                                <a 
+                                    className="" 
+                                    href="https://drive.google.com/file/d/1_oq6_eObSqUVOon_a-QFYhLeVUIk6qbh/view?usp=sharing" 
+                                    target="_blank" 
+                                    rel="noreferrer"
                                 >
-                                    <span><i className="fa fa-database mr-2"></i></span>
-                                    Download full database
-                                </button> : <p className='text-gray-300'>(Login to download the entire database)</p>
+                                    <button
+                                        // onClick={() => setIsOpen(true)}
+                                        className="w-full theme bg-gray-300 theme-text rounded-md text-lg px-4 py-3 hover:translate-y-1 outline-none"
+                                    >
+                                        <span><i className="fa fa-database mr-2"></i></span>
+                                        Download full database
+                                    </button>
+                                </a> : <p className='text-gray-300'>(Login to download the entire database)</p>
                         }
                     </Fragment>
                 </div>
